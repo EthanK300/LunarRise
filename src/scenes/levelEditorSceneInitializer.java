c0f3e1c71531427f133b7828baaf7009e87d8f10
RuntimeError <1bluejellybean@gmail.com>
2023-06-19 10:17:42 -0700

    refined

--- src/scenes/levelEditorSceneInitializer.java
+++ src/scenes/levelEditorSceneInitializer.java
@@ -187,7 +187,7 @@ public class levelEditorSceneInitializer extends SceneInitializer{
 		GameObject object = PreFabs.generateSpriteObject(sprite, 0.25f, 0.25f);//generated sprite size
 		RigidBody2D rb = new RigidBody2D();
 		//if the shift key is pressed it changes to a non-static object
-		if(keyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT) || mouseListener.mouseButtonDown(GLFW_KEY_RIGHT_SHIFT)) {
+		if(keyListener.isKeyPressed(GLFW_KEY_LEFT_SHIFT) || keyListener.isKeyPressed(GLFW_KEY_RIGHT_SHIFT)) {
 			rb.setBodyType(BodyType.Dynamic);
 			//System.out.println("pressed");
 		}else{
